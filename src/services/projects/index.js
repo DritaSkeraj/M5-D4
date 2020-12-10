42812f4b9a7eee100e85983ee1ef619b3de11a74
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2020-12-10 23:12:30 +0100

    fixes routes

--- src/services/projects/index.js
+++ src/services/projects/index.js
@@ -85,11 +85,12 @@ router.post(
   }
 )
 
+// delete project /projects/id
 router.delete("/:id", async (req, res, next) => {
   try {
     const usersDB = await readDB(projectsFilePath)
     const newDb = usersDB.filter(user => user.ID !== req.params.id)
-    await writeDB(projectsFilePath, newDB)
+    await writeDB(projectsFilePath, newDb)
 
     res.status(204).send()
   } catch (error) {
@@ -97,6 +98,7 @@ router.delete("/:id", async (req, res, next) => {
   }
 })
 
+// update project /projects/id
 router.put("/:id", async (req, res, next) => {
   try {
     const usersDB = await readDB(projectsFilePath)
@@ -109,7 +111,7 @@ router.put("/:id", async (req, res, next) => {
     }
 
     newDb.push(modifiedUser)
-    await writeDB(projectsFilePath, newDB)
+    await writeDB(projectsFilePath, newDb)
 
     res.send({ id: modifiedUser.ID })
   } catch (error) {
@@ -143,11 +145,17 @@ router.post("/:projectID/reviews", async (req, res, next) => {
         next(err)
       } else {
         const reviewsDB = await readDB(reviewsFilePath)
+        console.log("reviewsDB:::", reviewsDB);
+        console.log("reviewsFilePath::::",reviewsFilePath)
+
         const newReview = {
           ...req.body,
           ID: uniqid(),
+          projectID: req.params.projectID,
           addedAt: new Date(),
         }
+        console.log("newReview:::::::", newReview);
+        Promise.all([reviewsDB, newReview]);
 
         reviewsDB.push(newReview)
 
@@ -161,5 +169,57 @@ router.post("/:projectID/reviews", async (req, res, next) => {
   }
 )
 
+// GET /projects/id/reviews/id => get this specific review
+router.get("/:id/reviews/:rid", async (req, res, next) => {
+  try {
+    const reviewsDB = await readDB(reviewsFilePath)
+    const review = reviewsDB.filter(review => review.ID === req.params.rid && review.projectID === req.params.id)
+    if (review.length > 0) {
+      res.send(review)
+    } else {
+      const err = new Error()
+      err.httpStatusCode = 404
+      next(err)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// PUT /projects/id/reviews/id => update this review
+router.put("/:id/reviews/:rid", async (req, res, next) => {
+  try {
+    const reviewsDB = await readDB(reviewsFilePath)
+    const newDb = reviewsDB.filter(review => review.ID !== req.params.rid)
+    console.log("newDb.length:::::", newDb.length);
+    const modifiedReview = {
+      ...req.body,
+      ID: req.params.rid,
+      modifiedAt: new Date(),
+    }
+    console.log("modifiedReview::::::", modifiedReview);
+
+    await newDb.push(modifiedReview)
+    console.log("newDb.length:::::::::", newDb.length)
+    await writeDB(reviewsFilePath, newDb)
+
+    res.send({ id: modifiedReview.ID })
+  } catch (error) {
+    next(error)
+  }
+})
+
+// Delete review: /projects/id/reviews/id
+router.delete("/:id/reviews/:rid", async (req, res, next) => {
+  try {
+    const reviewsDB = await readDB(reviewsFilePath)
+    const newDb = reviewsDB.filter(review => review.ID !== req.params.rid)
+    await writeDB(reviewsFilePath, newDb)
+
+    res.status(204).send()
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router
